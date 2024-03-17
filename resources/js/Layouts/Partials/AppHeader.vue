9f7e932778338f3a057f40575a5e28f32ee0e0a4
Ali Reda <alyredagomaa@gmail.com>
2024-03-17 09:59:22 +0200

    fix : fixing nav humberger toggling

--- resources/js/Layouts/Partials/AppHeader.vue
+++ resources/js/Layouts/Partials/AppHeader.vue
@@ -3,8 +3,6 @@ import { Head, Link, usePage } from '@inertiajs/vue3';
 import { computed, ref, watchEffect } from 'vue';
 import AppNav from '@/Layouts/Partials/AppNav.vue';
 let isEn = computed(() => usePage().props.lang == 'en');
-watchEffect( () => {
-})
 let showNav = ref(true); 
 </script>
 <template>
@@ -14,10 +12,10 @@ let showNav = ref(true);
                 {{ isEn ? "Connect" : "كونكت" }}
             </Link>
         </div>
-        <div class="justify-self-end border-2 border-white grid it-ce rounded-full p-2 w-[2rem] h-[2rem] mx-5" @click="showNav=!showNav">
-            <img src="/app-assets/icons/bars.svg" alt="bars" width="10px">
-        </div>
+        <button class="justify-self-end border-2 border-white grid it-ce rounded-full p-2 w-[2rem] h-[2rem] mx-5" >
+            <img src="/app-assets/icons/bars.svg" alt="bars" width="10px" @click="showNav=!showNav">
+        </button>
     </div>
-    <AppNav v-show="showNav" />
+    <AppNav v-if="showNav" />
     <div class="my-5"></div> 
 </template>
