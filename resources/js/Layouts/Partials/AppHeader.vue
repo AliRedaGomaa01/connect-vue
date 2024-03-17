eb1a33386f9b71266526065e5538c5c0cd6655fc
Ali Reda <alyredagomaa@gmail.com>
2024-03-17 10:35:12 +0200

    chore :  ( editing bar icon for nav showtoggle )

--- resources/js/Layouts/Partials/AppHeader.vue
+++ resources/js/Layouts/Partials/AppHeader.vue
@@ -4,6 +4,7 @@ import { computed, ref, watchEffect } from 'vue';
 import AppNav from '@/Layouts/Partials/AppNav.vue';
 let isEn = computed(() => usePage().props.lang == 'en');
 let showNav = ref(true); 
+let toggleShow = ()=> {showNav.value = !showNav.value;}
 </script>
 <template>
     <div class="w-full bg-main min-h-[6rem] grid grid-cols-2 items-center " >
@@ -12,10 +13,12 @@ let showNav = ref(true);
                 {{ isEn ? "Connect" : "كونكت" }}
             </Link>
         </div>
-        <button class="justify-self-end border-2 border-white grid it-ce rounded-full p-2 w-[2rem] h-[2rem] mx-5" >
-            <img src="/app-assets/icons/bars.svg" alt="bars" width="10px" @click="showNav=!showNav">
+        <button class="justify-self-end border-4 border-main hover:border-white grid it-ce rounded-full p-2 w-[3rem] h-[3rem] mx-5" @click="toggleShow()" >
+            <img src="/app-assets/icons/bars.svg" alt="bars" width="20px" >
         </button>
     </div>
-    <AppNav v-if="showNav" />
+    <div class="" v-if="showNav">
+        <AppNav  />
+    </div>
     <div class="my-5"></div> 
 </template>
