a09f55f0e05263de3e569017e0cbdf967afc8a51
Ali Reda <alyredagomaa@gmail.com>
2024-03-19 16:14:14 +0200

    fix :  ( fixing passed paramter to the gate function issue )

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -22,7 +24,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('isOwner', function ($id) {
+        Gate::define('isOwner', function (User $user , String $id) {
             return auth()->id() == $id;
         });
     }
